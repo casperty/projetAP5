15f887d00631403f4c0b42fe364f49205886191d
KalSkirata <guillaume.taylor@gmail.com>
2013-12-23 12:40:35 +0100

    Update SaveFile.java
    SaveFile qui marche, je mettrai l'OpenFile quand il sera fonctionnel

--- src/views/SaveFile.java
+++ src/views/SaveFile.java
@@ -6,10 +6,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+
+import models.Forme;
+import models.Model;
 /**
  * 
  * @author Fran�ois Lamothe Guillaume Leccoq Alexandre Ravaux
@@ -19,11 +23,13 @@ import javax.swing.JOptionPane;
 public class SaveFile {
 	private JFileChooser fileDialog;
 	private File fileName,selectedFile; 
+	private Model model;
 	/**
 	 * Fenetre pour sauvegarder le fichier, 
 	 * se lance quand on clique sur Ctrl+S ou bien File > Save
 	 */
-	public SaveFile(){
+	public SaveFile(Model model){
+		this.model=model;
 		if(fileDialog==null){
 			fileDialog = new JFileChooser();
 			File selectedFile;
@@ -38,7 +44,7 @@ public class SaveFile {
 	    int option = fileDialog.showSaveDialog(fileDialog);
 		if (option != JFileChooser.APPROVE_OPTION) return;  // Annuler ou fermeture de la fenetre.
 	         selectedFile = fileDialog.getSelectedFile();
-	         if (selectedFile.exists()) {  // Le fichier existe déjà devons nous nous ecraser le fichier existant ?
+	         if (selectedFile.exists()) {  // Le fichier existe déjà, devons nous ecraser le fichier existant ?
 	            int response = JOptionPane.showConfirmDialog( fileDialog,
 	                  "Le fichier \"" + selectedFile.getName()
 	                  + "\" existe déjà.\nVoulez-vous le remplacez ?", 
@@ -51,24 +57,25 @@ public class SaveFile {
 	    try{
 	    	FileWriter stream=new FileWriter(selectedFile);
 	    	out=new PrintWriter(stream);
-	    }catch(Exception e){ //Echec de l'ouverture du fihier
+	    }catch(Exception e){ // Echec de l'ouverture du fihier
 	    	JOptionPane.showMessageDialog(fileDialog, this,"Nous sommes desole mais une erreur s'est produite.", option);
 	    }
 	    try {
 	    		//entete du fichier
-	            out.println("AFG file");
+	      /*      out.println("AFG file");
 	            out.println("Path : "+selectedFile);
-	            out.println("Date of creation : "+recupDatActu());
-	            /*
-	            	Contenu du fichier >> out.println(...);
-	            */
+	            out.println("Date of creation : "+recupDatActu());*/
+	            // Contenu du fichier (liste des formes + leurs attributs)
+	            for(int i=0; i<model.getFormes().size();i++){
+	              	out.println(model.getFormes().get(i).toString());
+	            }
 	            out.close();
 	            if (out.checkError())
 	               throw new IOException("Output error.");
 	            fileName = selectedFile;
 	            //this.setTitle("AFG : " + fileName.getName());
 	         }
-	         catch (Exception e) { //Echec de l'enregistrement des donnees
+	         catch (Exception e) { // Echec de l'enregistrement des donnees
 	            JOptionPane.showMessageDialog(fileDialog, this,
 	               "Nous sommes desole mais une erreur s'est produite.\n" + e, option);
 	         }   
