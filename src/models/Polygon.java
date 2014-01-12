de8020b9d86b131f5fbefb9895c2e97593b120fb
Irrek <lamothefrancois@sfr.fr>
2014-01-12 12:27:37 +0100

    resize poly

--- src/models/Polygon.java
+++ src/models/Polygon.java
@@ -1,10 +1,12 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Polygon extends Forme {
 	
 	private int resizeRectid=-1;
+	private HashMap<Coord, float[]> ratio;
 
 	public Polygon(Coord pos,ColorModel color, boolean fill) {
 		super(color, fill);
@@ -12,7 +14,7 @@ public class Polygon extends Forme {
 		this.sz=new Coord(0,0);
 		points = new ArrayList<Coord>();
 		points.add(new Coord(pos));
-		updatePos();
+		updatePosSz();
 	}
 	
 	public Forme clone(){
@@ -21,7 +23,7 @@ public class Polygon extends Forme {
 		for(Coord p : this.getPoints()){
 			l.points.add(new Coord(p));
 		}
-		l.updatePos();
+		l.updatePosSz();
 		l.onMouseReleased(new Coord(0,0));
 		return l;
 	}
@@ -40,31 +42,37 @@ public class Polygon extends Forme {
 		}
 	}
 	
-	public void updatePos(){
-		for(Coord c : points){
-			if(c.getX()<pos.getX()){
-				pos.setX(c.getX());
-			}else if(c.getY()<pos.getY()){
-				pos.setY(c.getY());
+	public void updatePosSz(){
+		//recalcul pos/sz
+		Coord min = new Coord(9999,9999);
+		Coord max = new Coord(0,0);
+		for(Coord c1 : getPoints()){
+			if(c1.getX()<min.getX()){
+				min.setX(c1.getX());
+			}
+			if(c1.getY()<min.getY()){
+				min.setY(c1.getY());
+			}
+			if(c1.getX()>max.getX()){
+				max.setX(c1.getX());
+			}
+			if(c1.getY()>max.getY()){
+				max.setY(c1.getY());
 			}
 		}
-		updateSz();
+		pos.set(min);
+		sz=Coord.dif(max, pos);
 	}
 	
-	public void updateSz(){
-		for(Coord c : points){
-			if(c.getX()-pos.getX()>sz.getX()){
-				sz.setX(c.getX()-pos.getX());
-			}else if(c.getY()-pos.getY()>sz.getY()){
-				sz.setY(c.getY()-pos.getY());
-			}
-		}
-	}
-
 	@Override
 	public void onMouseDragged(Coord c) {
 		if(created){
-			moveTo(c);
+			if((isResize() && onResizeRect(c)!=-1) || resizeRectid!=-1){
+				resizeRectid=(resizeRectid==-1)?onResizeRect(c):resizeRectid;
+				resize(c);
+			}else if(isSelect()){
+				moveTo(c);
+			}
 		}
 	}
 
@@ -82,10 +90,10 @@ public class Polygon extends Forme {
 		if(!created){
 			if((c.getX()>=pos.getX()-5 && c.getX()<=pos.getX()+5) && (c.getY()>=pos.getY()-5 && c.getY()<=pos.getY()+5) && points.size()>1){
 				created=true;
-				updatePos();
+				updatePosSz();
 			}else{
 				points.add(new Coord(c));
-				updatePos();
+				updatePosSz();
 			}
 		}
 	}
@@ -104,18 +112,27 @@ public class Polygon extends Forme {
 
 	@Override
 	public void resize(Coord c) {
-		Coord nPos=new Coord(pos);
-		Coord nSz=new Coord(sz);
-		
+		if(created && ratio==null){
+			ratio = new HashMap<Coord, float[]>();
+			for(Coord p : getPoints()){
+				//ratio
+				float x,y;
+				x=((float)p.getX())/((float)sz.getX());
+				y=((float)p.getY())/((float)sz.getY());
+				System.out.println(x+" "+y);
+				float[] r = {x,y};
+				ratio.put(p, r);
+			}
+		}
 		ArrayList<Coord> pts = (ArrayList<Coord>) getRectBounds();
-		if(c.getX()<nPos.getX()){
+		if(c.getX()<pos.getX()){
 			resizeRectid=(resizeRectid==1)?0:(resizeRectid==2)?3:resizeRectid;
-		}else if(c.getX()>nPos.getX()+nSz.getX()){
+		}else if(c.getX()>pos.getX()+sz.getX()){
 			resizeRectid=(resizeRectid==0)?1:(resizeRectid==3)?2:resizeRectid;
 		}
-		if(c.getY()<nPos.getY()){
+		if(c.getY()<pos.getY()){
 			resizeRectid=(resizeRectid==0)?3:(resizeRectid==1)?2:resizeRectid;
-		}else if(c.getY()>nPos.getY()+nSz.getY()){
+		}else if(c.getY()>pos.getY()+sz.getY()){
 			resizeRectid=(resizeRectid==3)?0:(resizeRectid==2)?1:resizeRectid;
 		}
 		int prev = (resizeRectid==0)?3:resizeRectid-1;
@@ -131,7 +148,7 @@ public class Polygon extends Forme {
 			pts.get(next).setX(c.getX());
 		}
 		
-		//recalcul nPos/nSz
+		//recalcul pos/sz
 		Coord min = new Coord(9999,9999);
 		Coord max = new Coord(0,0);
 		for(Coord c1 : pts){
@@ -148,8 +165,47 @@ public class Polygon extends Forme {
 				max.setY(c1.getY());
 			}
 		}
-		nPos.set(min);
-		nSz=Coord.dif(max, nPos);
+		pos.set(min);
+		sz=Coord.dif(max, pos);
+		
+		
+
+		//update coord w ratio
+		for(Coord p : ratio.keySet()){
+			float[] r = ratio.get(p);
+			if(resizeRectid==0){
+				p.setX((int) (((1-r[0])*((float)sz.getX()))));
+				p.setY((int) (((1-r[1])*((float)sz.getY()))));
+			}
+			p.setX((int) ((r[0]*((float)sz.getX()))));
+			p.setY((int) ((r[1]*((float)sz.getY()))));
+		}
+		
+		int xm=getPoints().get(0).getX();
+		int ym=getPoints().get(0).getX();
+		for(Coord p : getPoints()){
+			if(p.getX()>xm)xm=p.getX();
+			if(p.getY()>ym)ym=p.getY();
+		}
+		int difx=xm-pts.get(1).getX();
+		int dify=ym-pts.get(1).getY();
+		for(Coord p : getPoints()){
+			p.setX(p.getX()-difx);
+			p.setY(p.getY()-dify);
+		}
+		
+		int xmin=getPoints().get(0).getX();
+		int ymin=getPoints().get(0).getX();
+		for(Coord p : getPoints()){
+			if(p.getX()<xmin)xmin=p.getX();
+			if(p.getY()<ymin)ymin=p.getY();
+		}
+		int difxmin=pts.get(3).getX()-xmin;
+		int difymin=pts.get(3).getY()-ymin;
+		for(Coord p : getPoints()){
+			p.setX(p.getX()+difxmin);
+			p.setY(p.getY()+difymin);
+		}
 	}
 
 }
